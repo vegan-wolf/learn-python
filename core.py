5a28807a99f3998d1c9ac6d693e330e1eda81998
Alexey.Ponimash <Alexey.Ponimash@billing.ru>
2020-05-24 06:28:10 +0300

    House object

--- core.py
+++ core.py
@@ -1,22 +1,33 @@
 import time
+import random
 import pygame
 import asyncio
 import executor
 
 FPS = 30
+random.seed(time.time())
 
 
-class Car(pygame.sprite.Sprite):
+class MoveableObject(pygame.sprite.Sprite):
     def __init__(self, surface, position, screen, *groups):
-        super(Car, self).__init__(*groups)
+        super(MoveableObject, self).__init__(*groups)
         self.screen = screen
         self.orig_image = surface
         self.image = self.orig_image
         self.rect = self.image.get_rect(center=position)
+        self.rect.clamp_ip(screen.get_rect())
         self.angle = 0
         self.direction = pygame.math.Vector2(0, -1)
         self.speed = 0
         self.cell_width = max(self.rect.width, self.rect.height)
+
+    @property
+    def x(self):
+        return self.rect.centerx
+
+    @property
+    def y(self):
+        return self.rect.centery
 
     def rotate(self, angle):
         self.angle = self.angle + angle
@@ -96,8 +107,12 @@ def main():
     screen = pygame.display.set_mode((800, 600))
 
     sprites = pygame.sprite.Group()
-    car = Car(pygame.image.load('data/car.png'), (33, 300), screen, sprites)
+    car = MoveableObject(pygame.image.load('data/car.png'), (33, 300), screen, sprites)
     car.rotate(90)
+
+    screen_rect = screen.get_rect()
+    position = (random.randint(0, screen_rect.width), random.randint(0, screen_rect.height))
+    #house = MoveableObject(pygame.image.load('data/house.png'), position, screen, sprites)
 
     asyncio.ensure_future(pygame_loop(screen, sprites))
     loop.run_in_executor(None, executor.task, car)
